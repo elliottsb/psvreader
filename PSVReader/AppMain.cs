fdc23d8bc344be0f880171d8c060c05dcc850a2b
150273112 <150273112@qq.com>
2012-05-04 13:14:58 +0000

    zxy:提交空白资源工程

--- PSVReader/AppMain.cs
+++ PSVReader/AppMain.cs
@@ -5,6 +5,7 @@ using Sce.Pss.Core;
 using Sce.Pss.Core.Environment;
 using Sce.Pss.Core.Graphics;
 using Sce.Pss.Core.Input;
+using Sce.Pss.HighLevel.UI;
 
 namespace PSVReader
 {
@@ -27,12 +28,25 @@ namespace PSVReader
 		{
 			// Set up the graphics system
 			graphics = new GraphicsContext ();
+			
+			// TODO: Create scenes and call UISystem.SetScene
+			// Create scene			
+			UISystem.Initialize(graphics);
+			
+			// Set scene
+			UISystem.SetScene(MyScene, null);
 		}
 
 		public static void Update ()
 		{
 			// Query gamepad for current state
 			var gamePadData = GamePad.GetData (0);
+			
+			// Query touch for current state
+            List<TouchData> touchDataList = Touch.GetData (0);
+
+            // Update UI Toolkit
+            UISystem.Update(touchDataList);
 		}
 
 		public static void Render ()
@@ -40,7 +54,10 @@ namespace PSVReader
 			// Clear the screen
 			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
 			graphics.Clear ();
-
+			
+			// Render UI Toolkit
+            UISystem.Render ();
+			
 			// Present the screen
 			graphics.SwapBuffers ();
 		}
