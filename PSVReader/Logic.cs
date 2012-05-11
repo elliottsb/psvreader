3a8f39cedfc4082d007a9bd3cd9e28f71d6df2b4
150273112 <150273112@qq.com>
2012-05-11 14:24:44 +0000

    zxy:修改文件名称

---
+++ PSVReader/Logic.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Text;
+using Sce.Pss.HighLevel.UI;
+using System.Collections.Generic;
+
+
+namespace PSVReader
+{
+	public class UIOperator
+	{
+		private static string storyName;
+		
+		public UIOperator ()
+		{
+			
+		}
+		
+		public static void testSaveFile()
+		{	
+			string tempstr = "this is test string";
+			
+			ASCIIEncoding encode = new ASCIIEncoding();
+			
+			PSVReader.FileManage.SaveFile(encode.GetBytes(tempstr), "file1", "chapter1");
+		}
+		
+		public static void DownloadStory(string name, string chapter, string url)
+		{
+			testSaveFile();
+			//PSVReader.FileDownloadMgr.DownloadStory(name, chapter, url);
+		}
+
+		public static void SetStoryName(string name)
+		{
+			storyName = name;
+		}
+		
+		public static string GetStroyName()
+		{
+			return storyName;
+		}
+		
+		public static void ShowOneStory(string name, string chapter, Label label)
+		{
+			FileStorage onefile = PSVReader.FileManage.GetStoryChapter(name, chapter);
+			
+			byte[] data = onefile.ReadContent();
+			
+			ASCIIEncoding encoding = new ASCIIEncoding();
+			label.Text = encoding.GetString(data);
+		}
+		
+		public static void UpdateStory(PSVReaderUI.StoryNameList list)
+		{
+			List<string> names = FileManage.GetAllStoryName();
+			
+			foreach (string na in names)
+			{
+				List<string> chapterName = FileManage.GetStory(na);
+				
+				list.AddBook(na, chapterName);
+			}				
+		}
+	}
+}
+
